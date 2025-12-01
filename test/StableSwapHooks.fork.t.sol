7a0ce0b675ba9f020c07c5d28e127c6adf76d0a3
Nando <24811313+fzavalia@users.noreply.github.com>
2025-12-01 12:43:11 -0300

    fix: Calculate sqrtprice in tests

--- test/StableSwapHooks.fork.t.sol
+++ test/StableSwapHooks.fork.t.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.30;
 
 import {Test} from "forge-std/Test.sol";
+import {IERC20} from "forge-std/interfaces/IERC20.sol";
 import {Currency} from "@uniswap/v4-core/src/types/Currency.sol";
 import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
 import {PoolId} from "@uniswap/v4-core/src/types/PoolId.sol";
@@ -10,6 +11,7 @@ import {IHooks} from "@uniswap/v4-core/src/interfaces/IHooks.sol";
 import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
 import {IPositionManager} from "@uniswap/v4-periphery/src/interfaces/IPositionManager.sol";
 import {HookMiner} from "@uniswap/v4-periphery/src/utils/HookMiner.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {StableSwapHooks} from "src/StableSwapHooks.sol";
 
 contract StableSwapHooksForkTest is Test {
@@ -23,6 +25,8 @@ contract StableSwapHooksForkTest is Test {
         address poolManager = 0x000000000004444c5dc75cB358380D2e3dE08A90;
         address token0 = 0x6B175474E89094C44Da98b954EedeAC495271d0F; // dai
         address token1 = 0xdAC17F958D2ee523a2206206994597C13D831ec7; // usdt
+        uint256 decimals0 = 18;
+        uint256 decimals1 = 6;
 
         // Hooks flags based on getHookPermissions()
         uint160 flags = Hooks.BEFORE_INITIALIZE_FLAG | Hooks.AFTER_ADD_LIQUIDITY_FLAG
@@ -47,8 +51,8 @@ contract StableSwapHooksForkTest is Test {
         // Verify variables have been initialized correctly
         assertEq(address(hooks.poolManager()), poolManager);
         assertEq(hooks.amp(), initialAmp);
-        assertEq(hooks.rate0(), 1e18); // 18 decimals
-        assertEq(hooks.rate1(), 1e30); //  6 decimals
+        assertEq(hooks.rate0(), 10 ** (36 - decimals0)); // 1e18 for 18 decimals
+        assertEq(hooks.rate1(), 10 ** (36 - decimals1)); // 1e30 for 6 decimals
         assertEq(
             PoolId.unwrap(hooks.poolId()),
             PoolId.unwrap(
@@ -74,7 +78,7 @@ contract StableSwapHooksForkTest is Test {
                     tickSpacing: hooks.TICK_SPACING(),
                     hooks: IHooks(address(hooks))
                 }),
-                hooks.SQRT_PRICE_1_1()
+                uint160(Math.sqrt((10 ** decimals1 << 192) / 10 ** decimals0)) // sqrtPriceX96 for 1:1 value ratio
             );
 
         // Validate that the tick is not failure
