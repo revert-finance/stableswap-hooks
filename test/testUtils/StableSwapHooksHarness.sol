a9cb8ab1b42b18b695ac298d5f7627bbedf4f09b
Nando <24811313+fzavalia@users.noreply.github.com>
2025-12-05 17:34:31 -0300

    fix: Add internal functions to harness

--- test/testUtils/StableSwapHooksHarness.sol
+++ test/testUtils/StableSwapHooksHarness.sol
@@ -28,4 +28,20 @@ contract StableSwapHooksHarness is StableSwapHooks {
             _lpFeePercentage
         )
     {}
+
+    function handleWithdrawProtocolFeesCallback() external {
+        _handleWithdrawProtocolFeesCallback();
+    }
+
+    function handleWithdrawHookFeesCallback(bytes memory data) external {
+        _handleWithdrawHookFeesCallback(data);
+    }
+
+    function getFees(uint256 _amount) external view returns (uint256 lpFees, uint256 hookFees, uint256 protocolFees) {
+        return _getFees(_amount);
+    }
+
+    function addFees(bool _isCurrency0, uint256 _protocolFees, uint256 _hookFees) external {
+        _addFees(_isCurrency0, _protocolFees, _hookFees);
+    }
 }
