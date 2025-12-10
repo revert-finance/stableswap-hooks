1aca89577a45dc4c78bf696d25686ab05c6a25cf
Nando <24811313+fzavalia@users.noreply.github.com>
2025-12-10 15:29:11 -0300

    feat: Update base with hook permissions and _beforeInit hook

--- src/Base.sol
+++ src/Base.sol
@@ -6,6 +6,7 @@ import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
 import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
 import {PoolId} from "@uniswap/v4-core/src/types/PoolId.sol";
 import {IHooks} from "@uniswap/v4-core/src/interfaces/IHooks.sol";
+import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
 
 import {AccessControlEnumerable} from "@openzeppelin/contracts/access/extensions/AccessControlEnumerable.sol";
 
@@ -15,6 +16,9 @@ import {StableSwapMath} from "src/libraries/StableSwapMath.sol";
 
 /// @notice Abstract base contract for StableSwap hooks providing core state and configuration
 abstract contract Base is BaseHook, AccessControlEnumerable {
+    /// @notice Thrown when the operation is attempted on a pool that doesn't match this hook's poolId
+    error InvalidPoolId();
+
     /// @notice Fixed tick spacing used for the pool
     /// @dev We are not using concentrated liquidity so this value is just to form the pool key
     int24 public constant TICK_SPACING = 1;
@@ -68,5 +72,41 @@ abstract contract Base is BaseHook, AccessControlEnumerable {
         rate1 = StableSwapMath.getRate(_currency1);
 
         _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory permissions) {
+        permissions = Hooks.Permissions({
+            beforeInitialize: true,
+            afterInitialize: false,
+            beforeAddLiquidity: true,
+            afterAddLiquidity: false,
+            beforeRemoveLiquidity: true,
+            afterRemoveLiquidity: false,
+            beforeSwap: true,
+            afterSwap: false,
+            beforeDonate: true,
+            afterDonate: false,
+            beforeSwapReturnDelta: true,
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
+    }
+
+    /// @notice Validates that the given pool key matches this hook's poolId
+    /// @dev Reverts with InvalidPoolId if the pool ID doesn't match
+    /// @param _poolKey The pool key to validate
+    function _validatePoolId(PoolKey calldata _poolKey) internal view {
+        if (PoolId.unwrap(poolId) != PoolId.unwrap(_poolKey.toId())) {
+            revert InvalidPoolId();
+        }
+    }
+
+    /// @notice Validates pool initialization parameters.
+    /// @dev Reverts if the pool ID doesn't match.
+    function _beforeInitialize(address, PoolKey calldata _poolKey, uint160) internal view override returns (bytes4) {
+        _validatePoolId(_poolKey);
+
+        return BaseHook.beforeInitialize.selector;
     }
 }
