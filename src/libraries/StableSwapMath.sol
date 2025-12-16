b3ec0591e89f8e76e7156597709de8a913c92fd7
Nando <24811313+fzavalia@users.noreply.github.com>
2025-12-16 10:51:07 -0300

    fix: Remove comment

--- src/libraries/StableSwapMath.sol
+++ src/libraries/StableSwapMath.sol
@@ -77,54 +77,6 @@ library StableSwapMath {
         revert ConvergenceNotReached();
     }
 
-    // def get_D(_xp: DynArray[uint256, MAX_COINS], _amp: uint256) -> uint256:
-    //     """
-    //     D invariant calculation in non-overflowing integer operations
-    //     iteratively
-
-    //     A * sum(x_i) * n**n + D = A * D * n**n + D**(n+1) / (n**n * prod(x_i))
-
-    //     Converging solution:
-    //     D[j+1] = (A * n**n * sum(x_i) - D[j]**(n+1) / (n**n prod(x_i))) / (A * n**n - 1)
-    //     """
-    //     S: uint256 = 0
-    //     for x in _xp:
-    //         S += x
-    //     if S == 0:
-    //         return 0
-
-    //     D: uint256 = S
-    //     Ann: uint256 = _amp * N_COINS
-
-    //     for i in range(255):
-
-    //         D_P: uint256 = D
-    //         for x in _xp:
-    //             D_P = D_P * D / x
-    //         D_P /= pow_mod256(N_COINS, N_COINS)
-    //         Dprev: uint256 = D
-
-    //         # (Ann * S / A_PRECISION + D_P * N_COINS) * D / ((Ann - A_PRECISION) * D / A_PRECISION + (N_COINS + 1) * D_P)
-    //         D = (
-    //             (unsafe_div(Ann * S, A_PRECISION) + D_P * N_COINS) * D
-    //             /
-    //             (
-    //                 unsafe_div((Ann - A_PRECISION) * D, A_PRECISION) +
-    //                 unsafe_add(N_COINS, 1) * D_P
-    //             )
-    //         )
-
-    //         # Equality with the precision of 1
-    //         if D > Dprev:
-    //             if D - Dprev <= 1:
-    //                 return D
-    //         else:
-    //             if Dprev - D <= 1:
-    //                 return D
-    //     # convergence typically occurs in 4 rounds or less, this should be unreachable!
-    //     # if it does happen the pool is borked and LPs can withdraw via `remove_liquidity`
-    //     raise
-
     /// @notice Compute the missing reserves given the other reserves and invariant.
     /// @dev Rearranges the invariant into a quadratic and applies Newton-Raphson on the unknown reserves.
     /// @param _knownReserves The known reserves after a swap (scaled to 1e18 decimals).
