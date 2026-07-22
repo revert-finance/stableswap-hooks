9bd7096b1ec9caa0ea52f0c50a6364832b32fb8b
Nando <24811313+fzavalia@users.noreply.github.com>
2026-07-22 13:31:48 -0300

    feat(oracles): add ChainlinkOracleAdapter with L2 sequencer check

---
+++ src/oracles/ChainlinkOracleAdapter.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.30;
+
+contract ChainlinkOracleAdapter {
+    AggregatorV3Interface public immutable priceFeed;
+
+    uint256 public immutable priceFeedUpdatedAtTolerance;
+
+    uint8 public immutable priceFeedDecimals;
+
+    AggregatorV3Interface public immutable sequencerFeed;
+
+    uint256 public immutable sequencerFeedStartedAtGracePeriod;
+
+    error PriceFeedInvalidUpdatedAt();
+
+    error PriceFeedInvalidAnswer();
+
+    error SequencerFeedInvalidAnswer();
+
+    error SequencerFeedInvalidStartedAt();
+
+    constructor(
+        AggregatorV3Interface _priceFeed,
+        uint256 _priceFeedUpdatedAtTolerance,
+        AggregatorV3Interface _sequencerFeed,
+        uint256 _sequencerFeedStartedAtGracePeriod
+    ) {
+        priceFeed = _priceFeed;
+        priceFeedUpdatedAtTolerance = _priceFeedUpdatedAtTolerance;
+        priceFeedDecimals = _priceFeed.decimals();
+
+        sequencerFeed = _sequencerFeed;
+        sequencerFeedStartedAtGracePeriod = _sequencerFeedStartedAtGracePeriod;
+    }
+
+    function getRate() external view returns (uint256) {
+        (, int256 sequencerFeedAnswer, uint256 sequencerFeedStartedAt,,) = sequencerFeed.latestRoundData();
+
+        // 0 means the sequencer is working, another value means it is not.
+        if (sequencerFeedAnswer != 0) {
+            revert SequencerFeedInvalidAnswer();
+        }
+
+        // revert if the started at has default value.
+        if (sequencerFeedStartedAt == 0) {
+            revert SequencerFeedInvalidStartedAt();
+        }
+
+        // reverts if the sequencer has been started too recently.
+        if (block.timestamp - sequencerFeedStartedAt <= sequencerFeedStartedAtGracePeriod) {
+            revert SequencerFeedInvalidStartedAt();
+        }
+
+        (, int256 priceFeedAnswer,, uint256 priceFeedUpdatedAt,) = priceFeed.latestRoundData();
+
+        // reverts if the price returned is invalid.
+        if (priceFeedAnswer <= 0) {
+            revert PriceFeedInvalidAnswer();
+        }
+
+        // reverts if the answer is too old.
+        if (block.timestamp - priceFeedUpdatedAt > priceFeedUpdatedAtTolerance) {
+            revert PriceFeedInvalidUpdatedAt();
+        }
+
+        return 1e18 * uint256(priceFeedAnswer) / (10 ** priceFeedDecimals);
+    }
+}
+
+interface AggregatorV3Interface {
+    function decimals() external view returns (uint8);
+
+    function latestRoundData()
+        external
+        view
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
